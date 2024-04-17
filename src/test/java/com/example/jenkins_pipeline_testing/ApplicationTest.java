26c39e6b5a29036c87f70ad02900bdd4594b7dba
Jackson Reed <jackson.s.reed@gmail.com>
2024-04-17 12:51:19 -0500

    Adding dummy tests.

---
+++ src/test/java/com/example/jenkins_pipeline_testing/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.example.jenkins_pipeline_testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @Test
+    void stubTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void stubTest2() {
+        assertTrue(true);
+    }
+
+}
